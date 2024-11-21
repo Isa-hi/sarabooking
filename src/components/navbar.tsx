d3f537632b68063fd57addadf83cf2b263104ab9
Héctor Isahi Ochoa <lacasademiamigochanel@gmail.com>
2024-11-21 10:28:39 -0600

    Add user session handling in Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,8 @@ import { Button } from "./ui/button";
 
 export default function Navbar() {
   const router = useRouter();
+  const user = localStorage.getItem("user");
+
   return (
     <>
       <nav className="flex items-center justify-between p-4 bg-primary text-primary-foreground">
@@ -11,19 +13,34 @@ export default function Navbar() {
           <a href="/">✨ SaraBooking </a>{" "}
         </h1>
         <div className="gap-4 flex">
-          <Button
-            variant="secondary"
-            onClick={() => router.push("/auth/register")}
-          >
-            Registrarse
-          </Button>
-          <Button
-            variant="ghost"
-            className="border"
-            onClick={() => router.push("/auth/login")}
-          >
-            Iniciar sesión
-          </Button>
+          {user?.length ? (
+            <Button
+              variant="secondary"
+              onClick={() => {
+                localStorage.removeItem("user");
+                window.location.reload()
+              }}
+            >
+              {" "}
+              Cerrar sesión
+            </Button>
+          ) : (
+            <>
+              <Button
+                variant="secondary"
+                onClick={() => router.push("/auth/register")}
+              >
+                Registrarse
+              </Button>
+              <Button
+                variant="ghost"
+                className="border"
+                onClick={() => router.push("/auth/login")}
+              >
+                Iniciar sesión
+              </Button>
+            </>
+          )}
         </div>
       </nav>
     </>
